8e70142013b6ffb007117a1328e76a6d5978fd6c
Oliver Stigley <oliverstigley@gmail.com>
2017-11-01 22:13:54 +1300

    start of hough circles.
    needs tinkering min dat

---
+++ python/circles.py
@@ -0,0 +1,19 @@
+# https://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
+import numpy as np
+import cv2
+img = cv2.imread('./src/images/catan-board-original.jpg', 0)
+
+img = cv2.medianBlur(img,5)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,40,
+                            param1=50,param2=30,minRadius=0,maxRadius=0)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+cv2.imwrite('./src/results/catan-circles.jpg',cimg)
