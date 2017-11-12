bda9652746344016aecedec60af215536b6c9816
Oliver Stigley <oliver@boost.co.nz>
2017-11-12 17:00:25 +1300

    circles sorted in to rows and columns

--- python/circles.py
+++ python/circles.py
@@ -8,9 +8,18 @@ cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
 circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,200,
                             param1=50,param2=30,minRadius=20,maxRadius=30)
-
+# round
 circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
+
+# Sort in to hexes and their rows
+circles = sorted(circles[0], key=lambda x: x[1])
+row1 = sorted([circles[0], circles[1], circles[2]], key=lambda x: x[0])
+row2 = sorted([circles[3], circles[4], circles[5], circles[6]], key=lambda x: x[0])
+row3 = sorted([circles[7], circles[8], circles[9], circles[10]], key=lambda x: x[0])
+row4 = sorted([circles[11], circles[12], circles[13], circles[14]], key=lambda x: x[0])
+row5 = sorted([circles[15], circles[16], circles[17]], key=lambda x: x[0])
+
+for i in circles:
     # draw the outer circle
     cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
     # draw the center of the circle
